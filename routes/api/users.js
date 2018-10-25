edc798e555493a6a7365ec8ee3db6571e8e2406c
Roman Danilov <r.danylov@gmail.com>
2018-10-25 20:30:37 +0300

    Login/Regiter validation finished

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 
 //Load iput Validation
 const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 //Load user Model
 const User = require("../../models/User");
@@ -23,9 +24,9 @@ router.get("/test", (req, res) => {
 // @desc   Register user
 // @access Public
 router.post("/register", (req, res) => {
-  const { errors, isValid, name, email, password } = validateRegisterInput(
-    req.body
-  );
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  const { name, email, password } = req.body;
 
   // Check Validation
   if (!isValid) return res.status(400).json(errors);
@@ -67,12 +68,18 @@ router.post("/register", (req, res) => {
 // @desc   Login User / Reurning JWT Token
 // @access Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) return res.status(400).json(errors);
+
   const { email, password } = req.body;
   User.findOne({ email })
     .then(user => {
       //Check for user
       if (!user) {
-        return res.status(404).json({ email: "User not found" });
+        errors.email = "User not found";
+        return res.status(404).json(errors);
       }
 
       // Check password
@@ -92,7 +99,8 @@ router.post("/login", (req, res) => {
             }
           );
         } else {
-          return res.status(400).json({ password: "Password is incorrect" });
+          errors.password = "Password is incorrect";
+          return res.status(400).json(errors);
         }
       });
     })
--- validation/register.js
+++ validation/register.js
@@ -3,8 +3,42 @@ const isEmpty = require("./is-empty");
 
 module.exports = validateRegisterInput = data => {
   let errors = {};
-  if ((!Validator.isLength(data.name), { min: 3, max: 30 })) {
+
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : ""; // Password Confirmation
+
+  if (!Validator.isLength(data.name, { min: 3, max: 30 })) {
     errors.name = "Name must be beween 3 and 30 character";
+  }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name field is required";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 character";
+  }
+
+  if (!Validator.equals(data.password2, data.password)) {
+    errors.password2 = "Passwords must match";
+  }
+
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm Password field is required";
   }
 
   return {
