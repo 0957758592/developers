066f4775f5fde680734f7673b0d767681f1bc37d
Roman Danilov <r.danylov@gmail.com>
2018-10-27 01:08:30 +0300

    Posts Model, API's (GET,POST,DELETE) &  Validation added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,100 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+//Post Model
+const Post = require("../../models/Post");
+//Profile Model
+const Profile = require("../../models/Profile");
+
+//Validation
+const validatePostInput = require("../../validation/post");
 
 //@route  GET api/posts/test
 //@desc   Tests post route
 //@access Public
-router.get('/test', (req, res)=>{
-    res.json({msg: 'Posts'});
-})
+router.get("/test", (req, res) => {
+  res.json({ msg: "Posts" });
+});
+
+//@route    GET api/posts
+//@desc     Get all posts
+//@access   Public
+
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: "No any posts found" }));
+});
+
+//@route    GET api/posts/:id
+//@desc     Get post by ID
+//@access   Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: "No post found with that ID" })
+    );
+});
+
+//@route    DELETE api/posts/:id
+//@desc     Delete post by ID
+//@access   Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id)
+          .then(post => {
+            //Check for post owner
+            if (post.user.toString() !== req.user.id) {
+              return res
+                .status(401)
+                .json({ notauthorized: "User not authorized" });
+            }
+            // Delete
+            post.remove().then(() => res.json({ success: true }));
+          })
+          .catch(err =>
+            res.status(404).json({ postnotfound: "No post found" })
+          );
+      })
+      .catch(err =>
+        res
+          .status(404)
+          .json({ noaccess: "You have no access to delete this post" })
+      );
+  }
+);
+
+//@route    POST api/posts
+//@desc     Create post
+//@access   Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    //Check Validation
+    if (!isValid) {
+      //if any errors, send 400 status with errors object
+      return res.status(400).json(errors);
+    }
+
+    const { text, name, avatar } = req.body;
+    const newPost = new Post({
+      text: text,
+      name: name,
+      avatar: avatar,
+      user: req.user.id
+    });
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
